15727ebf9308855c19390f073e50b5497aaae252
Sharnalk <anas.jiyar1@gmail.com>
2026-01-01 20:41:10 +0100

    correction map blocking prod build

--- components/map/map-france.tsx
+++ components/map/map-france.tsx
@@ -1,6 +1,7 @@
 "use client";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+
 import paca from "@/lib/departements/region-provence-alpes-cote-d-azur.json";
 import occitanie from "@/lib/departements/region-occitanie.json";
 import auvRhoneAlpe from "@/lib/departements/region-auvergne-rhone-alpes.json";
@@ -11,11 +12,13 @@ export default function MapFrance() {
   return (
     <div className="aspect-video rounded-xl bg-slate-900 border border-slate-800 flex items-center justify-center">
       <MapContainer
-        center={centerFrance}
-        zoom={5}
-        wheelPxPerZoomLevel={1500}
-        style={{ height: "100%", width: "100%" }}
-        scrollWheelZoom={true}
+
+        bounds={[
+          [51.1, -5.1],
+          [41.3, 9.6],
+        ]}
+        boundsOptions={{ padding: [100, 100] }}
+        style={{ height: "150%", width: "100%", borderRadius: "12px" }}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <GeoJSON data={paca} />
