f2c36fd0e400d57942817aa1e1118da23acddf3d
Sharnalk <anas.jiyar1@gmail.com>
2026-01-01 21:00:47 +0100

    correction import type geojson blocking prod buiild

--- components/map/map-france.tsx
+++ components/map/map-france.tsx
@@ -1,14 +1,11 @@
 "use client";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
-
+import type { GeoJsonObject } from "geojson";
 import paca from "@/lib/departements/region-provence-alpes-cote-d-azur.json";
 import occitanie from "@/lib/departements/region-occitanie.json";
 import auvRhoneAlpe from "@/lib/departements/region-auvergne-rhone-alpes.json";
-
 export default function MapFrance() {
-  const centerFrance = [46.2276, 2.2137];
-
   return (
     <div className="aspect-video rounded-xl bg-slate-900 border border-slate-800 flex items-center justify-center">
       <MapContainer
@@ -21,9 +18,9 @@ export default function MapFrance() {
         style={{ height: "150%", width: "100%", borderRadius: "12px" }}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <GeoJSON data={paca} />
-        <GeoJSON data={occitanie} />
-        <GeoJSON data={auvRhoneAlpe} />
+        <GeoJSON data={paca as GeoJsonObject} />
+        <GeoJSON data={occitanie as GeoJsonObject} />
+        <GeoJSON data={auvRhoneAlpe as GeoJsonObject} />
       </MapContainer>
     </div>
   );
